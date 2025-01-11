26d70fcd221ac7d73af3627bb76b158b805adcbb
Orhan AKDOĞAN <orhan2akdogan@gmail.com>
2025-01-11 09:22:07 +0300

    Update FindRPartial_Standart.py

--- FindRPartial_Standart.py
+++ FindRPartial_Standart.py
@@ -1,5 +1,9 @@
 #!pip install ecdsa
 
+# Finding the K value that contains part of the R value (Standart)
+SearchStartKvalue = 1
+FindRPartial = 123
+
 from ecdsa import SECP256k1
 
 def CreateR(k: int):
@@ -8,8 +12,8 @@ def CreateR(k: int):
     P = k * G
     r = P.x() % n
     return r
-
-def find_k_with_prefix(prefix="123", start_k=1):
+    
+def find_k_with_prefix(prefix, start_k):
     k = start_k
     while True:
         r = CreateR(k)
@@ -17,9 +21,10 @@ def find_k_with_prefix(prefix="123", start_k=1):
         if r_hex.startswith(prefix):
         #if prefix in r_hex:
         #if r_hex.endswith(prefix):
-            return k, r_hex
+            return k,r, r_hex
         k += 1
 
-k, r_hex = find_k_with_prefix()
-print(f"Found k: {k}")
-print(f"R(HEX): {r_hex}")
+k,r, r_hex = find_k_with_prefix(FindRPartial, SearchStartKvalue)
+print(f"Found K: {k}")
+print(f"R: {r}")
+print(f"R (hex): {r_hex}")
